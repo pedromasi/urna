93f1fb9c6660db9f49524dbb3c95632e2ac731c4
Pedro Machado da Silva <pedro_masi@yahoo.com.br>
2023-07-06 23:53:32 -0300

    css finalizado, iniciando script JS

---
+++ phases.js
@@ -0,0 +1,50 @@
+let phases = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                name: 'fulano de tallo',
+                partido: 'ABC',
+                fotos:[
+                    {url: '38111.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: 77222,
+                name: 'beltrano da silva',
+                partido: 'DEF',
+                fotos:[
+                    {url: '77222.png', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                name: 'abubuleh',
+                partido: 'ABC',
+                vice: 'abu',
+                fotos:[
+                    {url: '99.png', legenda: 'Prefeito'},
+                    {url: '99_2.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: 84,
+                name: 'zulano',
+                partido: 'QWERTY',
+                vice: 'zul',
+                fotos:[
+                    {url: '84.jpg', legenda: 'Prefeito'},
+                    {url: '84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ] 
+            },
+        ]
+    }
+]
---
+++ script.js
@@ -0,0 +1,6 @@
+let seuVotoPara = document.querySelector('.textoinit span');
+let aCargo = document.querySelector('.cargo span');
+let descricao = document.querySelector('.restoInfos');
+let aviso = document.querySelector('.pBaixo');
+let lateral = document.querySelector('.dir');
+let numeros = document.querySelector('.numeros');
